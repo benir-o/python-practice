8247cb2e0e469a83884e828d82893a03109e6fe2
benir-o <beniromenda@gmail.com>
2025-04-25 12:41:58 +0300

    summing digits a user inputs

---
+++ week3 assignment/summing_digits.py
@@ -0,0 +1,12 @@
+number_input=input("Enter the number: ")
+
+def summing_the_number(number):
+    numlist=list(map(int,number))
+    accumulator=0
+    for item in numlist:
+        accumulator+=item
+    return accumulator
+print(summing_the_number(number_input))
+
+
+
